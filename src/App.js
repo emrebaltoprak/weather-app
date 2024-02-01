5e51ebfb5c7d210a939419b6d29295f97cc8899c
emrebaltoprak <toprakbalemre@gmail.com>
2024-02-02 00:57:51 +0100

    weatherApp

--- src/App.js
+++ src/App.js
@@ -1,23 +1,120 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
+import "./App.css";
+import Card from "./components/Card";
+import axios from "axios";
 
 function App() {
+  const [inputValue, setInputValue] = useState("ankara");
+
+  const handleInputChange = (event) => {
+    setInputValue(event.target.value);
+  };
+
+  const [data, setData] = useState("ankara");
+
+  const handleButtonChange = () => {
+    const apiKey = "V6AWZNVPM3LTHC8HK9GUNYAWJ";
+    axios
+      .get(
+        `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${inputValue}/?key=V6AWZNVPM3LTHC8HK9GUNYAWJ&unitGroup=metric&include=days`
+      )
+      .then((response) => {
+        setData(response.data);
+      })
+      .catch((error) => {
+        console.error("API request error:", error);
+      });
+  };
+
+  const handleKeyPress = (event) => {
+    if (event.key === "Enter") {
+      handleButtonChange();
+    }
+  };
+
+  const upper = `${inputValue[0].toUpperCase()}${inputValue
+    .slice(1)
+    .toLowerCase()}`;
+  console.log("data2", data);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div>
+      <div>
+        <h1>Weather App</h1>
+      </div>
+      <div className="inputTag">
+        <input
+          type="text"
+          placeholder="Enter The City Name"
+          value={inputValue}
+          onChange={handleInputChange}
+        />
+        <button
+          id="btn"
+          className="btn btn-outline-light"
+          onClick={handleButtonChange}
         >
-          Learn React
-        </a>
-      </header>
+          Search
+        </button>
+      </div>
+      <div>
+        <h2 className="text-center text-white">"{upper}"</h2>
+      </div>
+      <div className="row cardGroup">
+        {data && data.days && data.days.length > 0 && (
+          <>
+            <Card
+              cardTitle={data.days[0].datetime}
+              degree={data.days[0].temp}
+              himidity={data.days[0].humidity}
+              windspeed={data.days[0].windspeed}
+              icons={data.days[0].icon}
+            />
+            <Card
+              cardTitle={data.days[1].datetime}
+              degree={data.days[1].temp}
+              himidity={data.days[1].humidity}
+              windspeed={data.days[1].windspeed}
+              icons={data.days[1].icon}
+            />
+            <Card
+              cardTitle={data.days[2].datetime}
+              degree={data.days[2].temp}
+              himidity={data.days[2].humidity}
+              windspeed={data.days[2].windspeed}
+              icons={data.days[2].icon}
+            />
+            <Card
+              cardTitle={data.days[3].datetime}
+              degree={data.days[3].temp}
+              himidity={data.days[3].humidity}
+              windspeed={data.days[3].windspeed}
+              icons={data.days[3].icon}
+            />
+            <Card
+              cardTitle={data.days[4].datetime}
+              degree={data.days[4].temp}
+              himidity={data.days[4].humidity}
+              windspeed={data.days[4].windspeed}
+              icons={data.days[4].icon}
+            />
+            <Card
+              cardTitle={data.days[5].datetime}
+              degree={data.days[5].temp}
+              himidity={data.days[5].humidity}
+              windspeed={data.days[5].windspeed}
+              icons={data.days[5].icon}
+            />
+            <Card
+              cardTitle={data.days[6].datetime}
+              degree={data.days[6].temp}
+              himidity={data.days[6].humidity}
+              windspeed={data.days[6].windspeed}
+              icons={data.days[6].icon}
+            />
+          </>
+        )}
+      </div>
     </div>
   );
 }
---
+++ src/components/Card.js
@@ -0,0 +1,61 @@
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+function Card(props) {
+  let iconSrc;
+
+  if (props.icons === "clear-day") {
+    iconSrc = "Assets/sunny.png";
+  } else if (props.icons === "partly-cloudy-day") {
+    iconSrc = "Assets/partly-cloudy.png";
+  } else if (props.icons === "snow") {
+    iconSrc = "Assets/snowy.png";
+  } else if (props.icons === "rain") {
+    iconSrc = "Assets/rainy.png";
+  } else if (props.icons === "cloudy") {
+    iconSrc = "Assets/cloudy.png";
+  }
+
+  return (
+    <div className="card" style={{ width: 10 + "rem" }}>
+      <div className="card-body">
+        <h5 className="card-title">{props.cardTitle} </h5>
+
+        <p className="degree">{props.degree} °C</p>
+        <div className="imgfont">
+          <img className="imgTag" src={iconSrc} alt="image" />
+        </div>
+        <div className="windHumadity">
+          <div className="humidityTag">
+            <span className="humidity">{props.himidity}</span>
+
+            <div>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="icon"
+                viewBox="0 0 384 512"
+              >
+                <path d="M192 512C86 512 0 426 0 320C0 228.8 130.2 57.7 166.6 11.7C172.6 4.2 181.5 0 191.1 0h1.8c9.6 0 18.5 4.2 24.5 11.7C253.8 57.7 384 228.8 384 320c0 106-86 192-192 192zM96 336c0-8.8-7.2-16-16-16s-16 7.2-16 16c0 61.9 50.1 112 112 112c8.8 0 16-7.2 16-16s-7.2-16-16-16c-44.2 0-80-35.8-80-80z" />
+              </svg>
+            </div>
+          </div>
+
+          <div className="windTag">
+            <span className="wind">{props.windspeed}</span>
+            <div>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="icon"
+                viewBox="0 0 512 512"
+              >
+                <path d="M288 32c0 17.7 14.3 32 32 32h32c17.7 0 32 14.3 32 32s-14.3 32-32 32H32c-17.7 0-32 14.3-32 32s14.3 32 32 32H352c53 0 96-43 96-96s-43-96-96-96H320c-17.7 0-32 14.3-32 32zm64 352c0 17.7 14.3 32 32 32h32c53 0 96-43 96-96s-43-96-96-96H32c-17.7 0-32 14.3-32 32s14.3 32 32 32H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H384c-17.7 0-32 14.3-32 32zM128 512h32c53 0 96-43 96-96s-43-96-96-96H32c-17.7 0-32 14.3-32 32s14.3 32 32 32H160c17.7 0 32 14.3 32 32s-14.3 32-32 32H128c-17.7 0-32 14.3-32 32s14.3 32 32 32z" />
+              </svg>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Card;
